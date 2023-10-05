3323b0052c5b6c79350b5531944711b49f1e5375
Dmitriy <9266004760@bk.ru>
2023-10-05 10:56:30 +0300

    add second format

---
+++ format2.py
@@ -0,0 +1,17 @@
+str1 = 'Python'
+print("Welcome %s" % str1)
+print("Actual Number = %d" %15)
+print("Exponential equivalent of the number = %e" %15)
+print("Float of the number = %f" %15)
+print("Octal equivalent of the number = %o" %15)
+print("Hexadecimal equivalent of the number = %x" %15)
+str1 = 'World'
+str2 = ':'
+print("Python %s %s" %(str1,str2))
+print("Welcome to %%Python %s" %'language')
+print("Sunday\nMonday\nTuesday\nWednesday\nThursday\nFriday\nSaturday")
+print("""
+Language:
+\t1 Python
+\t2 Java\n\t3 JavaScript
+""")
